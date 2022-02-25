56bebf275902983258f43b395b5a4f91407acf8d
Kydyr bro <kydyrbro@MacBook-Pro-Sagynbek.local>
2022-02-25 22:41:33 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import javax.swing.plaf.basic.BasicTabbedPaneUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        calc(1,2,3,4,5,6);
+    }
+    static void calc(int ... asel) {
+        int couter = 0;
+        for (int a: asel) {
+            couter += a;
+        }
+        System.out.println(couter);
+    }
+}
+
